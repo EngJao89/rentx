dd7411f01ad35c28c679f8abfa484047114a9b9f
EngJao89 <eng_joao@outlook.com>
2022-10-01 00:19:06 -0300

    Closes 04:"Corrige logo e altera espaçamento."

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
+import { RFValue } from 'react-native-responsive-fontsize';
 
 import Logo from '../../assets/logo.svg';
 
-import { Container, Header } from './styles';
+import { 
+  Container, 
+  Header, 
+  TotalCars, 
+  HeaderContent 
+} from './styles';
 
 export function Home() {
   return (
@@ -13,9 +19,17 @@ export function Home() {
         backgroundColor="transparent"
         translucent
       />
-      <Header>
-        <Logo />
 
+      <Header>
+        <HeaderContent>
+          <Logo 
+            width={RFValue(108)}
+            height={RFValue(12)}
+          />
+          <TotalCars>
+            Total 12 Carros
+          </TotalCars>
+        </HeaderContent>
       </Header>
     </Container>
   );
--- mobile/src/screens/Home/styles.ts
+++ mobile/src/screens/Home/styles.ts
@@ -1,3 +1,5 @@
+import { RFValue } from 'react-native-responsive-fontsize';
+import { TouchableProps } from './../../../node_modules/react-native-svg/src/index.d';
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
@@ -11,4 +13,19 @@ export const Header = styled.View`
   height: 113px;
 
   background-color: ${({ theme }) => theme.colors.header};
+
+  justify-content: flex-end;
+  padding: 32px 24px;
+`;
+
+export const HeaderContent = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const TotalCars = styled.Text`
+  font-size: ${RFValue(15)}px;
+  font-family: ${({ theme }) => theme.fonts.primary_400};
+  color: ${({ theme }) => theme.colors.text};
 `;
