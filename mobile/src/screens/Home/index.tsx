954eb1133d9597bb5ff7068685c83480f84a9d86
EngJao89 <eng_joao@outlook.com>
2022-10-01 22:29:04 -0300

    Closes 05: "Implementa card de detalhes do veículo."

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -4,6 +4,9 @@ import { RFValue } from 'react-native-responsive-fontsize';
 
 import Logo from '../../assets/logo.svg';
 
+import { Car } from '../../components/Car';
+import { Price } from '../../components/Car/styles';
+
 import { 
   Container, 
   Header, 
@@ -12,6 +15,26 @@ import {
 } from './styles';
 
 export function Home() {
+  const carDataOne = {
+    brand: 'Audi',
+    name: 'RS 5 Coupé',
+    rent: {
+      period: 'AO DIA',
+      price: 120,
+    },
+    thumbnail: 'https://i.pinimg.com/originals/32/8a/05/328a05f1c3142221aed4f9c20fe9fef0.png'
+  }
+
+  const carDataTwo = {
+    brand: 'Porsche',
+    name: 'Panamera',
+    rent: {
+      period: 'AO DIA',
+      price: 340,
+    },
+    thumbnail: 'https://www.pngplay.com/wp-content/uploads/13/Porsche-Panamera-Transparent-Background.png'
+  }
+
   return (
     <Container >
       <StatusBar 
@@ -31,6 +54,10 @@ export function Home() {
           </TotalCars>
         </HeaderContent>
       </Header>
+
+      <Car data={carDataOne}/>
+      <Car data={carDataTwo}/>
+
     </Container>
   );
 }
