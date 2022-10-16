0e047187b4c63f3391f8f600ef20642dd9ba943a
EngJao89 <eng_joao@outlook.com>
2022-10-16 06:12:28 -0300

    Closes 32:"Melhora slides de fotos do carro."

--- mobile/src/components/ImageSlider/index.tsx
+++ mobile/src/components/ImageSlider/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { FlatList } from 'react-native';
+import React, { useState, useRef } from 'react';
+import { FlatList, ViewToken } from 'react-native';
 
 import { 
   Container, 
@@ -13,7 +13,18 @@ interface Props {
   imagesUrl: string[];
 }
 
+interface ChangeImageProps {
+  viewableItems: ViewToken[];
+  changed: ViewToken[];
+}
+
 export function ImageSlider({imagesUrl}: Props) {
+  const [imageIndex, setImageIndex] = useState(0); 
+
+  const indexChanged = useRef((info: ChangeImageProps) => {
+    const index = info.viewableItems[0].index!;
+    setImageIndex(index);
+  });
 
   return (
     <Container>
@@ -22,26 +33,29 @@ export function ImageSlider({imagesUrl}: Props) {
           imagesUrl.map((_, index) =>(
             <ImageIndex 
               key={String(index)}
-              active={true}
+              active={index === imageIndex}
             />
           ))
         }
       </ImageIndexes>
 
-      <CarImageWrapper>
-        <FlatList
-          data={imagesUrl}
-          keyExtractor={key => key}
-          renderItem={({ item }) => (
-            <CarImage
-              source={{ uri: imagesUrl[0] }} 
-              resizeMode="contain"
+      <FlatList
+        data={imagesUrl}
+        keyExtractor={key => key}
+        renderItem={({ item }) => (
+          <CarImageWrapper>
+            <CarImage 
+              source={{ 
+                uri: item                 
+              }}
+              resizeMode="contain"            
             />
-          )}
-          horizontal
-          showsHorizontalScrollIndicator={false}
-        />
-      </CarImageWrapper>
+          </CarImageWrapper>
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        onViewableItemsChanged={indexChanged.current}
+      />
     </Container>
   );
 }
