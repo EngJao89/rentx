19c9a76c211cc41b3bf9da7c01bbeff3eb658b13
EngJao89 <eng_joao@outlook.com>
2022-10-19 23:21:23 -0300

    Closes 37:"Implementa animação em Scroll."

--- mobile/src/screens/CarDetails/index.tsx
+++ mobile/src/screens/CarDetails/index.tsx
@@ -50,6 +50,35 @@ export function CarDetails() {
   const route = useRoute();
   const { car } = route.params as Params;
 
+  const theme = useTheme();
+
+  const scrollY = useSharedValue(0);
+  const scrollHandler = useAnimatedScrollHandler(event => {
+    scrollY.value = event.contentOffset.y;    
+  });
+
+  const headerStyleAnimation = useAnimatedStyle(() => {
+    return {
+      height: interpolate(
+        scrollY.value,
+        [0, 200],
+        [200, 70],
+        Extrapolate.CLAMP
+      ),
+    }
+  });
+
+  const sliderCarsStyleAnimation = useAnimatedStyle(() => {
+    return {
+      opacity: interpolate(
+        scrollY.value,
+        [0, 150],
+        [1, 0],
+        Extrapolate.CLAMP
+      )
+    }
+  });
+
   function handleConfirmRental() {
     navigation.navigate('Scheduling', { car: carUpdate });
   }
@@ -60,55 +89,73 @@ export function CarDetails() {
 
   return (
     <Container>
-      <Header>
-        <BackButton onPress={handleBack} />
-      </Header>
+      <Animated.View
+        style={[
+          headerStyleAnimation, 
+          styles.header,
+          { backgroundColor: theme.colors.background_secondary}
+        ]}
+      >
+        <Header>
+          <BackButton onPress={handleBack} />
+        </Header>
 
-      <CarImages>
-        <ImageSlider imagesUrl={['https://i.pinimg.com/originals/32/8a/05/328a05f1c3142221aed4f9c20fe9fef0.png']} />
-      </CarImages>
+        <Animated.View style={sliderCarsStyleAnimation}>
+          <CarImages>
+            <ImageSlider imagesUrl={['https://i.pinimg.com/originals/32/8a/05/328a05f1c3142221aed4f9c20fe9fef0.png']} />
+          </CarImages>
+        </Animated.View>
+      </Animated.View>
 
-      <Content>
-        <Details>
-          <Description>
-            <Brand>{car.brand}</Brand>
-            <Name>{car.name}</Name>
-          </Description>
+        <Content>
+          <Details>
+            <Description>
+              <Brand>{car.brand}</Brand>
+              <Name>{car.name}</Name>
+            </Description>
 
-          <Rent>
-            <Period>{car.period}</Period>
-            <Price>{car.price}</Price>
-          </Rent>
-        </Details>
+            <Rent>
+              <Period>{car.period}</Period>
+              <Price>{car.price}</Price>
+            </Rent>
+          </Details>
 
-        {
-          carUpdate.accessories &&
-          <Accessories>
-            {
-              carUpdate.accessories.map(accessory => (
-                <Accessory 
-                  key={accessory.type}
-                  name={accessory.name}
-                  icon={getAccessoryIcon(accessory.type)}
-                />
-              ))
-            }
-          </Accessories>
-        }
+          {
+            carUpdate.accessories &&
+            <Accessories>
+              {
+                carUpdate.accessories.map(accessory => (
+                  <Accessory 
+                    key={accessory.type}
+                    name={accessory.name}
+                    icon={getAccessoryIcon(accessory.type)}
+                  />
+                ))
+              }
+            </Accessories>
+          }
 
-        <About>
-          Este é automóvel desportivo. Surgio do lendário touro de lide indultado
-          na praça Real Maestranza de Sevilla. É um belíssimo carro para quem gosta 
-          de acelerar.
-        </About>
-      </Content>
+          <About>
+            Este é automóvel desportivo. Surgio do lendário touro de lide indultado
+            na praça Real Maestranza de Sevilla. É um belíssimo carro para quem gosta 
+            de acelerar.
+          </About>
+        </Content>
 
-      <Footer>
-      <Button 
-          title="Escolher período do aluguel" 
-          onPress={handleConfirmRental}
-        />
-      </Footer>
+        <Footer>
+        <Button 
+            title="Escolher período do aluguel" 
+            onPress={handleConfirmRental}
+          />
+        </Footer>
     </Container>
   );
 }
+
+const styles = StyleSheet.create({
+  header: {
+    position: 'absolute',
+    overflow: 'hidden', 
+    zIndex: 1,
+  },
+})
