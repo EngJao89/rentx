8561c206db0b10bba19e4d3cb7316abcf3adac4f
EngJao89 <eng_joao@outlook.com>
2022-10-31 23:56:45 -0300

    Closes 55:"Melhora Carros ."

--- mobile/src/components/Car/index.tsx
+++ mobile/src/components/Car/index.tsx
@@ -1,40 +1,40 @@
 import React from 'react';
-
-import { getAccessoryIcon } from '../../utils/getAccessoryIcon';
 import { RectButtonProps } from 'react-native-gesture-handler';
 
-import { CarDTO } from '../../dtos/CarDTO';
+import { getAccessoryIcon } from '../../utils/getAccessoryIcon';
+import { Car as ModelCar } from '../../database/model/Car';
 
-import {  
-  Container, 
-  Details, 
-  Brand, 
-  Name, 
-  About, 
-  Rent, 
-  Period, 
-  Price, 
+import {
+  Container,
+  Details,
+  Brand,
+  Name,
+  About,
+  Rent,
+  Period,
+  Price,
   Type,
   CarImage
 } from './styles';
 
+
 interface Props extends RectButtonProps{
-  data: CarDTO;
+  data: ModelCar;
 }
 
-export function Car({ data } : Props) {
+export function Car({ data, ...rest } : Props){
   const MotorIcon = getAccessoryIcon(data.fuel_type);
 
   return (
-    <Container>
+    <Container {...rest}>
       <Details>
         <Brand>{data.brand}</Brand>
         <Name>{data.name}</Name>
 
         <About>
           <Rent>
-            <Period>{data.rent.period}</Period>
-            <Price>{`R$ ${data.rent.price}`}</Price>
+            <Period>{data.period}</Period>
+            <Price>{`R$ ${data.price}`}</Price>
           </Rent>
 
           <Type>
@@ -47,7 +47,6 @@ export function Car({ data } : Props) {
         source={{ uri: data.thumbnail }} 
         resizeMode="contain"
       />
-
     </Container>
   );
 }
