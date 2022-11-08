8e05071d94de341311ed551d81ce4eb27434b7ea
EngJao89 <eng_joao@outlook.com>
2022-11-08 01:55:19 -0300

    Closes 69:"Alteração em Home e MyCars para melhorar sistema."

--- mobile/src/screens/Confirmation/styles.ts
+++ mobile/src/screens/Confirmation/styles.ts
@@ -4,6 +4,7 @@ import { RFValue } from 'react-native-responsive-fontsize';
 export const Container = styled.View`
   flex: 1;
   background-color: ${({ theme }) => theme.colors.header};
+
   padding-top: 96px;
 `;
 
@@ -11,6 +12,7 @@ export const Content = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
+
   padding-bottom: 80px;
 `;
 
@@ -18,6 +20,7 @@ export const Title = styled.Text`
   font-size: ${RFValue(30)}px;
   color: ${({ theme }) => theme.colors.shape};
   font-family: ${({ theme }) => theme.fonts.secondary_600};
+
   margin-top: 40px;
 `;
 
@@ -27,11 +30,13 @@ export const Message = styled.Text`
   font-family: ${({ theme }) => theme.fonts.primary_400};
   line-height:  ${RFValue(25)}px;
   text-align: center;
+
   margin-top: 16px;
 `;
 
 export const Footer = styled.View`
   width: 100%;
   align-items: center;
+
   margin: 80px 0;
 `;
