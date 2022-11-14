8dd3c3713b7ef9bd40b654a3ffa7b0855597b0aa
EngJao89 <eng_joao@outlook.com>
2022-11-14 17:15:53 -0300

    Closes 85:"Melhoras detalhes em meus carros."

--- mobile/src/screens/MyCars/index.tsx
+++ mobile/src/screens/MyCars/index.tsx
@@ -1,138 +1,105 @@
-import React, { useState, useEffect } from 'react';
-import { StatusBar, FlatList } from 'react-native';
-import { useTheme } from 'styled-components';
-import { useNavigation } from '@react-navigation/core';
-import { AntDesign } from '@expo/vector-icons';
-import { parseISO, format } from 'date-fns';
+import React, { useState, useEffect } from 'react'
+import { StatusBar, FlatList } from 'react-native'
+import { AntDesign } from '@expo/vector-icons'
 
-import { BackButton } from '../../components/BackButton';
-import { LoadAnimation } from '../../components/LoadAnimation';
+import { useHooks } from '../../Hooks/useHooks'
 
-import { Car } from '../../components/Car';
-import { CarDTO } from '../../dtos/CarDTO';
-import { Car as ModelCar } from '../../database/model/Car';
-import { api } from '../../services/api';
+import { api } from '../../services/api'
+import { Car as ModelCar } from '../../database/model/Car'
 
-import {
-  Container,
-  Header,
-  Title,
-  SubTitle,
-  Content,
-  Appointments,
-  AppointmentsTitle,
-  AppointmentsQuantity,
-  CarWrapper,
-  CarFooter,
-  CarFooterTitle,
-  CarFooterPeriod,
-  CarFooterDate,
-} from './styles';
+import { Car } from '../../components/Car'
+import { BackButton } from '../../components/BackButton'
+import { LoadAnimation } from '../../components/LoadAnimation'
 
-interface CarProps {
-  id: string;
-  user_id: string;
-  car: CarDTO;
-  startDate: string;
-  endDate: string;
+import * as S from './styles'
+import { formatDate } from '../../utils/formatted'
+
+type DataProps = {
+  id: string
+  car: ModelCar
+  start_date: string
+  end_date: string
 }
 
-interface DataProps {
-  id: string;
-  car: ModelCar; 
-  start_date: string;
-  end_date: string;  
-}
+export function MyCars() {
+  const [cars, setCars] = useState<DataProps[]>([])
+  const [loading, setLoading] = useState(true)
 
-export function MyCars(){
-  const [cars, setCars] = useState<DataProps[]>([]);
-  const [loading, setLoading] = useState(true);
+  const { navigation, theme, screenIsFocus } = useHooks()
 
-  const navigation = useNavigation();
-  const  theme = useTheme();
-
-  function handleBack(){
-    navigation.goBack();    
+  function handleBack() {
+    navigation.goBack()
   }
 
   useEffect(() => {
-    async function fetchCars(){
+    async function fetCars() {
       try {
-        const response = await api.get('/rentals');   
+        const response = await api.get('/rentals')
         const dataFormatted = response.data.map((data: DataProps) => {
           return {
+            id: data.id,
             car: data.car,
-            start_date: format(parseISO(data.start_date), 'dd/MM/yyyy'),
-            end_date: format(parseISO(data.end_date), 'dd/MM/yyyy'),
+            start_date: formatDate(data.start_date, 'digit'),
+            end_date: formatDate(data.end_date, 'digit')
           }
-        })           
-        setCars(dataFormatted);
+        })
+        setCars(dataFormatted)
       } catch (error) {
-        console.log(error);
-      } finally{
-        setLoading(false);
+        console.log(error)
+      } finally {
+        setLoading(false)
       }
     }
 
-    fetchCars();
-  },[]);
+    fetCars()
+  }, [screenIsFocus])
 
   return (
-    <Container>
-      <Header>
-        <StatusBar
-          barStyle="light-content"
-          translucent
-          backgroundColor="transparent"
-        />
-        <BackButton
-          onPress={handleBack} 
-          color={theme.colors.shape}
-        />
+    <S.Container>
+      <StatusBar barStyle="light-content" translucent backgroundColor="transparent" />
+      <S.Header>
+        <S.HeaderBackButton>
+          <BackButton onPress={handleBack} color={theme.colors.shape} />
+        </S.HeaderBackButton>
 
-        <Title>
-          Escolha uma {'\n'}
-          data de início e {'\n'}
-          fim do aluguel
-        </Title>
-
-        <SubTitle>
-          Conforto, segurança e praticidade.
-        </SubTitle>
-      </Header>
-      { 
-        loading ? <LoadAnimation /> :
-        <Content>
-          <Appointments>
-            <AppointmentsTitle>Agendamentos feitos</AppointmentsTitle>
-            <AppointmentsQuantity>{cars.length}</AppointmentsQuantity>
-          </Appointments>
-
-          <FlatList 
+        <S.TextWrapper>
+          <S.Title>Seus agendamentos, {'\n'}estão aqui.</S.Title>
+          <S.SubTitle>Conforto, segurança e praticidade.</S.SubTitle>
+        </S.TextWrapper>
+      </S.Header>
+      {loading ? (
+        <LoadAnimation />
+      ) : (
+        <S.Content>
+          <S.Appointments>
+            <S.AppointmentsTitle>Agendamentos feitos</S.AppointmentsTitle>
+            <S.AppointmentsQuantity>{cars.length}</S.AppointmentsQuantity>
+          </S.Appointments>
+          <FlatList
             data={cars}
-            keyExtractor={item => String(item.id)}
+            keyExtractor={item => item.id}
             showsVerticalScrollIndicator={false}
-            renderItem={({item}) => (
-              <CarWrapper>
+            renderItem={({ item }) => (
+              <S.CarWrapper>
                 <Car data={item.car} />
-                <CarFooter>
-                  <CarFooterTitle>Período</CarFooterTitle>
-                  <CarFooterPeriod>
-                    <CarFooterDate>{item.start_date}</CarFooterDate>
-                    <AntDesign 
+                <S.CarFooter>
+                  <S.CarFooterTitle>Período</S.CarFooterTitle>
+                  <S.CarFooterPeriod>
+                    <S.CarFooterDate>{item.start_date}</S.CarFooterDate>
+                    <AntDesign
                       name="arrowright"
                       size={20}
                       color={theme.colors.title}
                       style={{ marginHorizontal: 10 }}
                     />
-                    <CarFooterDate>{item.end_date}</CarFooterDate>
-                  </CarFooterPeriod>
-                </CarFooter>
-              </CarWrapper>
+                    <S.CarFooterDate>{item.end_date}</S.CarFooterDate>
+                  </S.CarFooterPeriod>
+                </S.CarFooter>
+              </S.CarWrapper>
             )}
-          /> 
-        </Content>
-      }
-    </Container>
-  );
+          />
+        </S.Content>
+      )}
+    </S.Container>
+  )
 }
