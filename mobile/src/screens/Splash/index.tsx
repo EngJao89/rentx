bf6347c490137ed8b38fa61f4d47b9b4a1df207e
EngJao89 <eng_joao@outlook.com>
2022-11-16 12:30:08 -0300

    Closes 91: "Altera splash screen."

--- mobile/src/screens/Splash/index.tsx
+++ mobile/src/screens/Splash/index.tsx
@@ -1,81 +1,67 @@
-import React, { useEffect } from 'react';
-import { useNavigation } from '@react-navigation/core';
+import React, { useEffect } from 'react'
 
-import BrandSvg from '../../assets/brand.svg';
-import LogoSvg from '../../assets/logo.svg';
+import { useHooks } from '../../Hooks/useHooks'
+import BrandSvg from '../../assets/brand.svg'
+import LogoSvg from '../../assets/logo.svg'
 
-import Animated, { 
-  useSharedValue, 
+import Animated, {
+  useSharedValue,
   useAnimatedStyle,
   withTiming,
-  interpolate,  
+  interpolate,
   Extrapolate,
   runOnJS
-}  from 'react-native-reanimated';
+} from 'react-native-reanimated'
 
-import {
-  Container
-} from './styles';
+import * as S from './styles'
 
-export function Splash(){  
-  const splashAnimation = useSharedValue(0);
+export function Splash() {
+  const splashAnimation = useSharedValue(0)
 
-  const navigation = useNavigation();
+  const { navigation } = useHooks()
 
   const brandStyle = useAnimatedStyle(() => {
     return {
-      opacity: interpolate(splashAnimation.value, [0, 50], [1,0]),
+      opacity: interpolate(splashAnimation.value, [0, 50], [1, 0]),
       transform: [
         {
-          translateX: interpolate(splashAnimation.value,
-            [0, 50],
-            [0, -50],
-            Extrapolate.CLAMP
-          )
+          translateX: interpolate(splashAnimation.value, [0, 50], [0, -50], Extrapolate.CLAMP)
         }
-      ],
+      ]
     }
-  });
+  })
 
   const logoStyle = useAnimatedStyle(() => {
     return {
-      opacity: interpolate(splashAnimation.value, [0, 25, 50], [0, .3, 1]),
+      opacity: interpolate(splashAnimation.value, [0, 25, 50], [0, 0.3, 1]),
       transform: [
         {
-          translateX: interpolate(splashAnimation.value, 
-            [0, 50],
-            [-50, 0],
-            Extrapolate.CLAMP
-          )
+          translateX: interpolate(splashAnimation.value, [0, 50], [-50, 0])
         }
-      ]      
+      ]
     }
-  });
+  })
 
   function startApp() {
-    navigation.navigate('SignIn');    
+    navigation.navigate('SignIn')
   }
 
   useEffect(() => {
-    splashAnimation.value = withTiming(
-      50, 
-      { duration: 1000 },
-      () => {
-        'worklet'
-        runOnJS(startApp)();
-      }
-    );
-  },[]);
+    splashAnimation.value = withTiming(50, { duration: 1000 }, () => {
+      'worklet'
+      runOnJS(startApp)()
+    })
+  }, [])
 
   return (
-    <Container>
-      <Animated.View style={[brandStyle, {position: 'absolute'}]}>
+    <S.Container>
+      <Animated.View style={[brandStyle, { position: 'absolute' }]}>
         <BrandSvg width={80} height={50} />
       </Animated.View>
 
-      <Animated.View style={[logoStyle, {position: 'absolute'}]}>
+      <Animated.View style={[logoStyle, { position: 'absolute' }]}>
         <LogoSvg width={180} height={20} />
       </Animated.View>
-    </Container>
-  );
+    </S.Container>
+  )
 }
