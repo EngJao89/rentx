5e267ccbc2b012a53deb1a3bf140fea6731965d0
EngJao89 <eng_joao@outlook.com>
2022-11-19 01:33:42 -0300

    Closes 100:"Melhora componente de botão."

--- mobile/src/components/Button/index.tsx
+++ mobile/src/components/Button/index.tsx
@@ -1,41 +1,38 @@
-import React from 'react';
-import { ActivityIndicator } from 'react-native';
-import { useTheme } from 'styled-components';
-import {  RectButtonProps } from 'react-native-gesture-handler';
-import {
-  Container,
-  Title
-} from './styles';
+import React from 'react'
+import { ActivityIndicator } from 'react-native'
+import { RectButtonProps } from 'react-native-gesture-handler'
 
-interface Props extends RectButtonProps{
-  title: string;
-  color?: string;
-  loading?: boolean;
-  light?: boolean;
+import { useHooks } from '../../Hooks/useHooks'
+import * as S from './styles'
+
+interface Props extends RectButtonProps {
+  title: string
+  color?: string
+  loading?: boolean
+  light?: boolean
 }
 
 export function Button({
   title,
   color,
-  onPress,
   enabled = true,
   loading = false,
   light = false,
-}: Props){
-  const theme = useTheme();
-
+  ...rest
+}: Props) {
+  const { theme } = useHooks()
   return (
-    <Container 
-      color={color ? color : theme.colors.main} 
-      onPress={onPress}
+    <S.Container
+      color={color}
       enabled={enabled}
-      style={{ opacity: (enabled === false || loading === true) ? .5 : 1 }}
+      style={{ opacity: enabled === false || loading === true ? 0.5 : 1 }}
+      {...rest}
     >
-      {
-        loading 
-        ? <ActivityIndicator color={theme.colors.shape} />
-        : <Title light={light}>{title}</Title>      
-      }
-    </Container>
-  );
+      {loading ? (
+        <ActivityIndicator color={theme.colors.shape} />
+      ) : (
+        <S.Title light={light}>{title}</S.Title>
+      )}
+    </S.Container>
+  )
 }
