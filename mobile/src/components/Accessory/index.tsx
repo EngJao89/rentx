140f400a05e2032f865ee731b9e543deddabec36
EngJao89 <eng_joao@outlook.com>
2022-11-18 23:50:31 -0300

    Closes 96:"Melhora acessórios e icones."

--- mobile/src/components/Accessory/index.tsx
+++ mobile/src/components/Accessory/index.tsx
@@ -1,31 +1,18 @@
-import React from 'react';
-import { SvgProps } from 'react-native-svg';
-import { useTheme } from 'styled-components';
+import React from 'react'
+import { SvgProps } from 'react-native-svg'
 
-import {
-  Container,
-  Name
-} from './styles';
+import * as S from './styles'
 
-interface Props {
-  name: string;
+type Props = {
+  name: string
   icon: React.FC<SvgProps>
 }
 
-export function Accessory({
-  name,
-  icon: Icon
-}:Props){
-  const theme = useTheme();
-
+export function Accessory({ name, icon: ICon }: Props) {
   return (
-    <Container>
-      <Icon 
-        width={32} 
-        height={32} 
-        fill={theme.colors.header} 
-      />
-      <Name>{name}</Name>
-    </Container>
-  );
+    <S.Container>
+      <ICon width={32} height={32} />
+      <S.Name>{name}</S.Name>
+    </S.Container>
+  )
 }
