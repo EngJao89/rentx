dcda65ff93f8dff14fa0994a17e4a06c87e3743f
EngJao89 <eng_joao@outlook.com>
2022-11-21 00:36:32 -0300

    Closes 104:"Altera e melhora botão de confimação."

--- mobile/src/components/ConfirmButton/styles.ts
+++ mobile/src/components/ConfirmButton/styles.ts
@@ -1,6 +1,6 @@
-import styled from 'styled-components/native';
-import { RFValue } from 'react-native-responsive-fontsize';
-import { RectButton } from 'react-native-gesture-handler';
+import styled, { css } from 'styled-components/native'
+import { RectButton } from 'react-native-gesture-handler'
+import { RFValue } from 'react-native-responsive-fontsize'
 
 export const Container = styled(RectButton)`
   width: 80px;
@@ -8,12 +8,13 @@ export const Container = styled(RectButton)`
 
   background-color: ${({ theme }) => theme.colors.shape_dark};
 
-  align-items: center;
   justify-content: center;
-`;
-
+  align-items: center;
+`
 export const Title = styled.Text`
-  font-family: ${({ theme }) => theme.fonts.primary_500};
-  color: ${({ theme }) => theme.colors.shape};
-  font-size: ${RFValue(15)}px;
-`;
+  ${({ theme }) => css`
+    font-family: ${theme.fonts.primary_500};
+    color: ${theme.colors.shape};
+    font-size: ${RFValue(15)}px;
+  `}
+`
