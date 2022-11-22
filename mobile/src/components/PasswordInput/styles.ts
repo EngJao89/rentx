393814d62521a76c88dbc9c8340073a9da1f5408
EngJao89 <eng_joao@outlook.com>
2022-11-22 13:20:12 -0300

    Closes 109:"Melhora input de password."

--- mobile/src/components/PasswordInput/styles.ts
+++ mobile/src/components/PasswordInput/styles.ts
@@ -1,40 +1,56 @@
-import styled, { css } from 'styled-components/native';
-import { RFValue } from 'react-native-responsive-fontsize';
-import { TextInput } from 'react-native';
+import styled, { css, DefaultTheme } from 'styled-components/native'
+import { RFValue } from 'react-native-responsive-fontsize'
+import { BorderlessButton } from 'react-native-gesture-handler'
 
-interface ContainerProps {
-  isFocused: boolean;
+type Props = {
+  isFocused: boolean
 }
 
-export const Container = styled.View<ContainerProps>`
+const focusPasswordVisibilityButton = {
+  focused: (theme: DefaultTheme) =>
+    css`
+      border-bottom-width: 2px;
+      border-bottom-color: ${theme.colors.main};
+    `
+}
+
+export const Container = styled.View`
   flex-direction: row;
-  margin-bottom: 8px;
+`
 
-  ${({ isFocused, theme }) => isFocused && css`
-    border-bottom-width: 2px;
-    border-bottom-color: ${theme.colors.main};    
-  `};
-`;
+export const IconContainer = styled.View<Props>`
+  ${({ theme, isFocused }) => css`
+    height: 56px;
+    width: 55px;
+    justify-content: center;
+    align-items: center;
+    margin-right: 2px;
+    background-color: ${theme.colors.background_secondary};
 
-export const IconContainer = styled.View`
-  height: 56px;
-  width: 55px;
-  justify-content: center;
-  align-items: center;
+    ${isFocused && focusPasswordVisibilityButton.focused(theme)};
+  `}
+`
+export const InputText = styled.TextInput<Props>`
+  ${({ theme, isFocused }) => css`
+    flex: 1;
+    padding: 0 23px;
+    color: ${theme.colors.text};
+    font-size: ${RFValue(15)}px;
+    font-family: ${theme.fonts.primary_400};
+    background-color: ${theme.colors.background_secondary};
 
-  margin-right: 2px;
+    ${isFocused && focusPasswordVisibilityButton.focused(theme)};
+  `}
+`
+export const PasswordVisibilityButton = styled(BorderlessButton)``
 
-  background-color: ${({ theme }) => theme.colors.background_secondary};
-`;
+export const ContainerPasswordVisibilityButton = styled.View<Props>`
+  ${({ theme, isFocused }) => css`
+    flex-direction: row;
+    align-items: center;
+    padding: 0 16px 0 16px;
+    background-color: ${theme.colors.background_secondary};
 
-
-export const InputText = styled(TextInput)`
-  flex: 1;
-
-  background-color: ${({ theme }) => theme.colors.background_secondary};
-  color: ${({ theme }) => theme.colors.text};
-  font-family: ${({ theme }) => theme.fonts.primary_400};
-  font-size: ${RFValue(15)}px;
-
-  padding: 0 23px;
-`;
+    ${isFocused && focusPasswordVisibilityButton.focused(theme)};
+  `}
+`
